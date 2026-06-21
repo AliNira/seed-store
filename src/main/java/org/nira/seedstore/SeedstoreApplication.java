4aef309743f564b5eb20262025d861de5799e302
AliNira <dev.erfaniii@gmail.com>
2026-06-21 05:02:08 -0800

    initial commit..

---
+++ src/main/java/org/nira/seedstore/SeedstoreApplication.java
@@ -0,0 +1,13 @@
+package org.nira.seedstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedstoreApplication.class, args);
+    }
+
+}
