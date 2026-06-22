eac6451463d86ae81fd3c546a8c65961268e5de0
AliNira <dev.erfaniii@gmail.com>
2026-06-22 04:58:18 -0800

    add Product.java and Category.java entities to project.

---
+++ src/main/java/org/nira/seedstore/Category.java
@@ -0,0 +1,18 @@
+package org.nira.seedstore;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private Set<Product> products;
+}
