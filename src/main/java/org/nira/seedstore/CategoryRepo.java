a10943ec9b2c176852c00c82660a97fa066b1bf7
AliNira <dev.erfaniii@gmail.com>
2026-06-23 06:19:05 -0800

    add CategoryController.java, CategoryService.java, CategoryRepo.java files and getCategories api

---
+++ src/main/java/org/nira/seedstore/CategoryRepo.java
@@ -0,0 +1,8 @@
+package org.nira.seedstore;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, Long> {
+}
