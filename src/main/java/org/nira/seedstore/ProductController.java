cbfad46f9be1e616f53e8128c99d76d04d74bf3e
AliNira <dev.erfaniii@gmail.com>
2026-06-25 11:00:59 -0800

    add getProducts and getProductsByCategory apis, edit Category api address.

---
+++ src/main/java/org/nira/seedstore/ProductController.java
@@ -0,0 +1,28 @@
+package org.nira.seedstore;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public List<Product> getProductsByCategory(@PathVariable Long categoryId) {
+        return productService.getAllProductsByCategory(categoryId);
+    }
+
+}
